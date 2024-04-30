e527dc82bb7b011d07cac5e6247448af2ed142f9
Martiinhz <once11caldas.martin@gmail.com>
2024-04-30 13:20:54 -0500

    PacmanLoader y estilos de las cards

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 export const Card = ({img , name , gender}) => {
   return (
-     <>
-    <div>
-        <img src={img} alt={name} />
-    </div>
-    <div>
-        <p>{name}</p>
+     
+<div className="flex items-center bg-gray-200 shadow-lg p-4 mb-4 rounded-lg">
+      <div className="mr-4">
+        <img className="w-32 h-32 object-cover rounded-lg" src={img} alt={name} />
+      </div>
+      <div>
+        <p className="text-xl font-semibold">{name}</p>
         <p>{gender}</p>
+      </div>
     </div>
-    </>
-  )
+  );
 }
